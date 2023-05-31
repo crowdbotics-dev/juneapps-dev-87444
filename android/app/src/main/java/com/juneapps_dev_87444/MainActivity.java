c29832d00a9e9112c0f0e856f6dbfc799a47c463
Crowdbotics <engineering@crowdbotics.com>
2023-05-31 06:23:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/juneapps_dev_87444/MainActivity.java
@@ -0,0 +1,15 @@
+package com.juneapps_dev_87444;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "juneapps_dev_87444";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "juneapps-dev-87444",
+  url: "https://juneapps-dev-87444.botics.co",
+  api: "https://juneapps-dev-87444.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
